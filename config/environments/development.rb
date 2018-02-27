dc179807e13309056b6297780f5915f4b528d55c
vague-z <app.engineer.z@gmail.com>
2018-02-27 11:41:04 +0900

    add letter_opener setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,15 +54,17 @@ Rails.application.configure do
 
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
   # mail setting
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV["GMAIL_USER_ID"],
-    :password => ENV["GMAIL_PASSWORD"],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => "smtp.gmail.com",
+  #   :port => 587,
+  #   :user_name => ENV["GMAIL_USER_ID"],
+  #   :password => ENV["GMAIL_PASSWORD"],
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 end
