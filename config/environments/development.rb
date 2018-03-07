3712feebb21858a87ff069e23b163f37146e7581
vague-z <app.engineer.z@gmail.com>
2018-03-07 10:50:40 +0900

    change develop env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
   # letter_opener
   config.action_mailer.delivery_method = :letter_opener_web
   # mail setting
